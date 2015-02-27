180dd4a576e1654dd65b46d51dd52a0ce27dcac2
James Dinh <jamesdinh14@gmail.com>
2015-02-26 22:52:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@ int main(int argc, char* argv) {
 	const int BOARD_SIZE = 8; // Othello is 8x8
 	char board[BOARD_SIZE][BOARD_SIZE] = { 0 }; // initialize board w/ 0
 	char currentPlayer = 1; // Black = 1, White = -1
-	int inRow, inCol;
+	int inRow = -2, inCol = -2;
 	bool gameOn = true; // Condition to keep game running
+	char passCount = 0; // Game ends when both players pass in succession
+	int turnCount = 0; // Keeps track of turns. Max # = 60
 
 	// START_COORDINATE marks the first 4 spots of Othello
 	const int START_COORDINATE_1 = 3, START_COORDINATE_2 = 4;
@@ -19,7 +21,7 @@ int main(int argc, char* argv) {
 	board[START_COORDINATE_1][START_COORDINATE_2] = 1;
 	board[START_COORDINATE_2][START_COORDINATE_1] = 1;
 
-	do { // 10 lines
+	do { // 12 lines
 		PrintBoard(board);
 
 		if (currentPlayer == 1)
@@ -27,14 +29,37 @@ int main(int argc, char* argv) {
 		else
 			cout << "White's turn." << endl;
 		cout << "Enter your move (row, col): " << endl;
+
 		GetMove(&inRow, &inCol);
 
+        // If both players pass in succession, the game ends
+		if (inRow == -1 && inCol == -1)
+			passCount++;
+		else
+			passCount = 0;
+
+		if (passCount == 2)
+			gameOn = false;
+
+        // If the game has taken 60+ turns, end
+		if (turnCount <= 60)
+			gameOn = false;
+
+        // Check for move validity before applying the move
 		if (IsValidMove(board, inRow, inCol)) {
 			ApplyMove(board, inRow, inCol, currentPlayer);
 			currentPlayer = -currentPlayer;
 		}
 		else
 			cout << "Invalid move. Try again" << endl;
-	} while (gameOn); // 23 lines
 
+	} while (gameOn); // 31 lines
+
+    // Determine the winner
+	if (GetValue(board) > 0)
+		cout << "Black is the winner.";
+	else if (GetValue(board) < 0)
+		cout << "White is the winner.";
+	else
+		cout << "The game is a tie."; // 36 lines
 }
