9f91c6bb1b55b8824bbdcd81e36d7e72740904fc
James Dinh <jamesdinh14@gmail.com>
2015-03-17 23:37:14 -0700

    Final update

--- OthelloGame.cpp
+++ OthelloGame.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 // Limit 15 lines
 // Display state of the board
-void PrintBoard(char board[8][8]) {
+void PrintBoard(char board[][BOARD_SIZE]) {
 
 	// Labeling of the columns on top
 	cout << " -  0  1  2  3  4  5  6  7  " << endl;
 
-	for (int row = 0; row < 8; row++) {
+	for (int row = 0; row < BOARD_SIZE; row++) {
 
 		// Labeling of rows on left side
 		cout << " " << row << " ";
 
-		for (int col = 0; col < 8; col++) {
+		for (int col = 0; col < BOARD_SIZE; col++) {
 
 			// Replace 0, 1, -1 w/ game markers
 			if (board[row][col] == 0)
@@ -32,14 +32,14 @@ void PrintBoard(char board[8][8]) {
 // return "true" if move is "in bounds"
 // Row and Col BOTH must be a value from 0-7
 bool InBounds(int row, int col) {
-	return (row <= 7 || row >= 0) && (col <= 7 || col >= 0);
+	return (row < BOARD_SIZE && row >= 0) && (col < BOARD_SIZE && col >= 0);
 }
 
 // Limit 3 lines
 // Check for validity
 // Must be "in bounds" and be an empty space
 // or be (-1, -1)/"pass" turn
-bool IsValidMove(char board[][8], int row, int col) {
+bool IsValidMove(char board[][BOARD_SIZE], int row, int col) {
 	return (InBounds(row, col) && board[row][col] == 0)
 		|| (row == -1 && col == -1);
 }
@@ -54,41 +54,42 @@ void GetMove(int *row, int *col) {
 
 // Limit 20 lines
 // Place piece on board and go through Othello's rules
-void ApplyMove(char board[][8], int row, int col, char currentPlayer) {
+void ApplyMove(char board[][BOARD_SIZE], int row, int col,
+	char currentPlayer) {
 	board[row][col] = currentPlayer; // place piece
-
-	int i = row, j = col; // temp position holders
 
 	// Delta's allow for 8-directional check
 	for (int rowDelta = -1; rowDelta <= 1; rowDelta++) {
-		for (int colDelta = -1; colDelta <= 1; colDelta++)
-		{
-			// Is the piece over an enemy piece?
-			if (board[row + rowDelta][col + colDelta] == -currentPlayer)
-			{
-				// Move over to the enemy piece
-                // Resets value of i and j
-				i = row + rowDelta;
-				j = col + colDelta;
+		for (int colDelta = -1; colDelta <= 1; colDelta++) {
 
-				// Loop that will keep stepping in one direction
-				for (;;)
-				{
-					i += rowDelta;
-					j += colDelta;
+			// If the check goes out-of-bounds, don't run
+			if (InBounds(row + rowDelta, col + colDelta)) {
 
-					// Stop and do nothing upon finding an empty space
-					if (board[i][j] == 0)
-						break;
+				// Check for enemy piece one space away
+				// If the deltas = 0, don't need to run
+				if (board[row + rowDelta][col + colDelta] == -currentPlayer 
+					&& !(rowDelta == 0 && colDelta == 0)) {
 
-					// If ally piece is found, begin backtracking and flipping
-					if (board[i][j] == currentPlayer)
-					{
-						while (board[i -= rowDelta][j -= colDelta]
-							== -currentPlayer)
-							board[i][j] = currentPlayer;
-						break; // 17 lines
-					}
+					// Move over to the enemy piece
+					// Resets value of i and j
+					int i = row + rowDelta;
+					int j = col + colDelta;
+
+					// As long as there is an enemy piece
+					// Keep moving in that direction
+					do {
+						i += rowDelta;
+						j += colDelta;
+
+						// When an ally piece is found,
+						// Backtrack and flip pieces
+						if (board[i][j] == currentPlayer) {
+							while (board[i -= rowDelta][j -= colDelta]
+								== -currentPlayer)
+								board[i][j] = currentPlayer;
+							break;
+						}
+					} while (board[i][j] == -currentPlayer); // 19 lines
 				}
 			}
 		}
@@ -97,13 +98,13 @@ void ApplyMove(char board[][8], int row, int col, char currentPlayer) {
 
 // Limit 5 lines
 // Return integer value of the board
-int GetValue(char board[][8]) {
-	int total = 0;
+int GetValue(char board[][BOARD_SIZE]) {
+	int total = 0; // temp value
 
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
 			total += board[i][j];
 		}
 	}
-	return total; // temp value
+	return total; // 5 lines
 }
